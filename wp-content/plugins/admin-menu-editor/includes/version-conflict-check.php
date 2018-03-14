03ad25bde2abe6f85c9209e10eafae1917737fe2
David Sanchez <david@dondavi.com>
2018-03-14 13:51:59 -0500

    fix version check editor.php

--- wp-content/plugins/admin-menu-editor/includes/version-conflict-check.php
+++ wp-content/plugins/admin-menu-editor/includes/version-conflict-check.php
@@ -1,52 +1,25 @@
-<<<<<<< HEAD
 <?php
 //It's not possible to have two versions of this plugin active at the same time. Abort plugin load
 //and display an error if we detect that another version has already been loaded.
 if ( class_exists('WPMenuEditor') ) {
 
-	function ws_ame_activation_conflict() {
-		if ( !current_user_can('activate_plugins') ) {
-			return; //The current user can't do anything about the problem.
-		}
-		?>
-		<div class="error fade">
-			<p>
-				<strong>Error: Another version of Admin Menu Editor is already active.</strong><br>
-				Please deactivate the older version. It is not possible to run two different versions
-				of this plugin at the same time.
-			</p>
-		</div>
-	<?php
-	}
+    function ws_ame_activation_conflict() {
+        if ( !current_user_can('activate_plugins') ) {
+            return; //The current user can't do anything about the problem.
+        }
+        ?>
+        <div class="error fade">
+            <p>
+                <strong>Error: Another version of Admin Menu Editor is already active.</strong><br>
+                Please deactivate the older version. It is not possible to run two different versions
+                of this plugin at the same time.
+            </p>
+        </div>
+        <?php
+    }
 
-	add_action('admin_notices', 'ws_ame_activation_conflict');
-	return true; //Conflict detected.
+    add_action('admin_notices', 'ws_ame_activation_conflict');
+    return true; //Conflict detected.
 }
 
-=======
-<?php
-//It's not possible to have two versions of this plugin active at the same time. Abort plugin load
-//and display an error if we detect that another version has already been loaded.
-if ( class_exists('WPMenuEditor') ) {
-
-	function ws_ame_activation_conflict() {
-		if ( !current_user_can('activate_plugins') ) {
-			return; //The current user can't do anything about the problem.
-		}
-		?>
-		<div class="error fade">
-			<p>
-				<strong>Error: Another version of Admin Menu Editor is already active.</strong><br>
-				Please deactivate the older version. It is not possible to run two different versions
-				of this plugin at the same time.
-			</p>
-		</div>
-	<?php
-	}
-
-	add_action('admin_notices', 'ws_ame_activation_conflict');
-	return true; //Conflict detected.
-}
-
->>>>>>> 676c6ed3ea2781a8ef08b445789c78df38727548
 return false; //No conflict.
